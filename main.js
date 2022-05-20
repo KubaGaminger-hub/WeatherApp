fe1b7e573ba2cb1011b0632977fb10eefce7de3f
Jakub <kuba.wezowicz@gmail.com>
2022-05-20 19:05:39 +0200

    Basics css Javascript better functionality

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const p = document.querySelector('p')
+const h2 = document.querySelector('h2')
 const submit = document.querySelector('input[name="submit"]')
 const city = document.querySelector('input[name="city"]')
 submit.addEventListener('click', () => {
@@ -6,6 +7,16 @@ submit.addEventListener('click', () => {
 
     fetch(URL)
     .then(res => res.json())
-    .then(data => console.log(data.currentConditions.temp))
-    .catch(err => console.log(err))
+    .then(data => {
+        h2.innerText = data.address
+        p.innerText = `${Math.ceil(data.currentConditions.temp)}°`
+    })
+    .catch(err => {
+        if (city.value != "") {
+            h2.innerText = 'Podałeś złe miasto!'
+        }else {
+            h2.innerText = 'Nie podałeś miasta!'
+        }
+        p.innerText = ""
+    })
 })
